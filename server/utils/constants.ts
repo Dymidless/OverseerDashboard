f33a0cbf8405d90ac367e06b66ddd0b97dfd4cae
Jhon Jairo Sosa Londoño <lonelydeathvgx@gmail.com>
2025-11-24 13:13:48 +0100

    feat(constants): `JWT_ENCODER` and `JWT_SECRET`

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -1,4 +1,9 @@
+const { jwtSecret } = useRuntimeConfig();
+
 export const DISCORD_API_BASE_URL = "https://discord.com/api/v10" as const;
 
 export const ENCRYPTION_ALGORITHM = "aes-256-gcm";
 export const ENCRYPTION_IV_LENGTH = 12;
+
+export const JWT_ENCODER = new TextEncoder();
+export const JWT_SECRET = JWT_ENCODER.encode(jwtSecret);
